69ef150c29c18781ce4ae279076ca2da2d333826
Jose Luis Represa <joseluis.represa@bbva.com>
2017-09-19 17:45:39 +0200

    export default config

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ require('babel-register');
 
 const Analyzer = require('./lib/analyzer');
 const parse = require('./lib/parse');
+const defaults = require('./lib/defaults');
 
 module.exports = {
 	Analyzer,
-	parse
+	parse,
+	defaults
 };
