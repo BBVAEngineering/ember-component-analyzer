d53a68091baf2a6b2cb6e7fdab251fa6bb5f3df3
Jose Luis Represa <joseluis.represa@bbva.com>
2018-08-07 12:14:17 +0200

    partial tests

---
+++ test/visit/partial-test.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const assert = require('assert');
+const process = require('../../lib/process');
+const visit = require('../../lib/visit');
+const DEFAULTS = require('../../lib/defaults');
+
+describe('visit.js', () => {
+	describe('#visit() - partials', () => {
+		it('should return partials', () => {
+			const input = process('test', `{{partial 'partial/path'}}`);
+			const result = visit(input.ast, DEFAULTS);
+
+			assert.deepEqual(result, [{
+				name: 'partial/path',
+				type: 'partial'
+			}]);
+		});
+	});
+});
